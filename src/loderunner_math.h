d16edf68325694a86b45f8b7b0fb8048226970a0
Ivan Ivanov <ivan@ivanovs.info>
2015-11-24 23:18:14 +1300

    Cleanup

--- src/loderunner_math.h
+++ src/loderunner_math.h
@@ -3,158 +3,102 @@
 
 #include <math.h>
 
-
-union v2
-{
-    struct
-    {
-        r32 x, y;
-    };
-    struct
-    {
-        r32 u, v;
-    };
-    r32 E[2];
+union v2 {
+  struct {
+    r32 x, y;
+  };
+  struct {
+    r32 u, v;
+  };
+  r32 E[2];
 };
 
+inline v2 operator*(r32 Scalar, v2 A) {
+  v2 Result;
 
-inline v2
-operator*(r32 Scalar, v2 A)
-{
-    v2 Result;
+  Result.x = A.x * Scalar;
+  Result.y = A.y * Scalar;
 
-    Result.x = A.x * Scalar;
-    Result.y = A.y * Scalar;
-
-    return Result;
+  return Result;
 }
 
+inline v2 operator*(v2 A, r32 Scalar) { return Scalar * A; }
 
-inline v2
-operator*(v2 A, r32 Scalar)
-{
-    return Scalar * A;
+inline v2 &operator*=(v2 &A, r32 Scalar) {
+  A = A * Scalar;
+
+  return A;
 }
 
+inline v2 operator+(v2 A, v2 B) {
+  v2 Result;
 
-inline v2 &
-operator*=(v2 &A, r32 Scalar)
-{
-    A = A * Scalar;
+  Result.x = A.x + B.x;
+  Result.y = A.y + B.y;
 
-    return A;
+  return Result;
 }
 
+inline v2 &operator+=(v2 &A, v2 B) {
+  A = A + B;
 
-inline v2
-operator+(v2 A, v2 B)
-{
-    v2 Result;
-
-    Result.x = A.x + B.x;
-    Result.y = A.y + B.y;
-
-    return Result;
+  return A;
 }
 
+inline v2 operator-(v2 A, v2 B) {
+  v2 Result;
 
-inline v2 &
-operator+=(v2 &A, v2 B)
-{
-    A = A + B;
+  Result.x = A.x - B.x;
+  Result.y = A.y - B.y;
 
-    return A;
+  return Result;
 }
 
+inline v2 &operator-=(v2 &A, v2 B) {
+  A = A - B;
 
-inline v2
-operator-(v2 A, v2 B)
-{
-    v2 Result;
-
-    Result.x = A.x - B.x;
-    Result.y = A.y - B.y;
-
-    return Result;
+  return A;
 }
-
-
-inline v2 &
-operator-=(v2 &A, v2 B)
-{
-    A = A - B;
-
-    return A;
-}
-
 
 // Unary
-inline v2
-operator-(v2 A)
-{
-    v2 Result;
+inline v2 operator-(v2 A) {
+  v2 Result;
 
-    Result.x = -A.x;
-    Result.y = -A.y;
+  Result.x = -A.x;
+  Result.y = -A.y;
 
-    return Result;
+  return Result;
 }
 
+inline r32 Square(r32 Real32) { return Real32 * Real32; }
 
-inline r32
-Square(r32 Real32)
-{
-    return Real32 * Real32;
+inline int Square(int Int) { return Int * Int; }
+
+inline r32 SquareRoot(r32 Real32) {
+  r32 Result = sqrtf(Real32);
+  return Result;
 }
 
-
-inline int
-Square(int Int)
-{
-    return Int * Int;
+inline r32 Abs(r32 Value) {
+  if (Value >= 0)
+    return Value;
+  else
+    return -Value;
 }
 
-
-inline r32
-SquareRoot(r32 Real32)
-{
-    r32 Result = sqrtf(Real32);
-    return Result;
+inline r32 V2Length(v2 Vector) {
+  r32 Result = SquareRoot(Square(Vector.x) + Square(Vector.y));
+  return Result;
 }
 
-
-inline r32
-Abs(r32 Value)
-{
-    if (Value >= 0)
-        return Value;
-    else
-        return -Value;
+inline r32 DistanceBetween(v2 Dot1, v2 Dot2) {
+  v2 Diff = Dot2 - Dot1;
+  return V2Length(Diff);
 }
 
-
-inline r32
-V2Length(v2 Vector)
-{
-    r32 Result = SquareRoot(Square(Vector.x) + Square(Vector.y));
-    return Result;
+inline r32 DotProduct(v2 Vector1, v2 Vector2) {
+  r32 Result = Vector1.x * Vector2.x + Vector1.y * Vector2.y;
+  return Result;
 }
-
-
-inline r32
-DistanceBetween(v2 Dot1, v2 Dot2)
-{
-    v2 Diff = Dot2 - Dot1;
-    return V2Length(Diff);
-}
-
-
-inline r32
-DotProduct(v2 Vector1, v2 Vector2)
-{
-    r32 Result = Vector1.x * Vector2.x + Vector1.y * Vector2.y;
-    return Result;
-}
-
 
 #endif
--- src/loderunner_platform.h
+++ src/loderunner_platform.h
@@ -1,12 +1,10 @@
 #ifndef LODERUNNER_PLATFORM_H
 #define LODERUNNER_PLATFORM_H
 
-
 #include <stdint.h>
 #include <stddef.h>
 #include <limits.h>
 #include <float.h>
-
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -25,12 +23,15 @@ typedef i32 bool32;
 #define local_persist static
 
 #if BUILD_SLOW
-#define Assert(Expression) if(!(Expression)) {*(int *)0 = 0;}
+#define Assert(Expression) \
+  if (!(Expression)) {     \
+    *(int *)0 = 0;         \
+  }
 #else
 #define Assert(Expression)
 #endif
 
-#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
-
+#define COUNT_OF(x) \
+  ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
 
 #endif  // LODERUNNER_PLATFORM_H
